bc5425ba4b4c2b953119d534552151c90038d523
SargisCH <sargis.chilingaryann@gmail.com>
2024-04-23 14:58:33 +0400

    neovim config updated

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,9 +4,17 @@ return {
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+		-- vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+		vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", {})
+		vim.keymap.set(
+			"n",
+			"<leader>fs",
+			"<cmd>Telescope grep_string<cr>",
+			{ desc = "find string under cursor in cwd" }
+		)
 	end,
 	defaults = {
 		file_ignore_patterns = { "node_modules" },
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,4 +5,10 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
 vim.g.mapleader = " "
 vim.opt.incsearch = true
+vim.opt.relativenumber = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.cursorline = true
+vim.opt.clipboard:append("unnamedplus") -- use sytem clipboard
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+
